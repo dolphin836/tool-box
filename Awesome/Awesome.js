5117a3661944914331cfc3052b61ba2a66861e48
whb <wanghaibing@shein.com>
2019-05-03 10:33:51 +0800

    更新首页，添加搜索、添加编译、添加收起展开

---
+++ Awesome/Awesome.js
@@ -0,0 +1,235 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var menu   = document.getElementById("menu"),
+        main   = document.getElementById("main"),
+        search = document.getElementById("search");
+    // 全局变量，所有的网站数据，用于搜索
+    var Data   = new Array();
+    // 搜索框获取焦点
+    search.focus();
+    // 初始化
+    init();
+
+    // 搜索事件
+    search.addEventListener("keyup", function (event) {
+        event.preventDefault();
+
+        if (search.value == '') {
+            init();
+            
+            return;
+        }
+
+        if (event.keyCode === 13) {
+            let data   = find(search.value);
+
+            let group  = new Object();
+
+            group.name = '搜索结果';
+            group.mark = '';
+            group.data = data;
+            // 隐藏网站列表
+            hideGroups();
+            // 添加搜索结果
+            addGroup(group);
+        }
+
+        if (event.keyCode === 27) {
+            search.value = '';
+            init();
+
+            return;
+        }
+    });
+
+    // 搜索
+    function find(text) {
+        let config = {
+            keys: [
+                'name',
+                'word'
+            ]
+        };
+
+        let fuse = new Fuse(Data, config)
+
+        return fuse.search(text);
+    }
+
+    // 隐藏网站列表
+    function hideGroups() {
+        let groupArr = main.children;
+
+        for (let i = 0; i < groupArr.length; i++) {
+            let isHidden = groupArr[i].classList.contains("is-hidden");
+
+            if (! isHidden) {
+                groupArr[i].classList.add('is-hidden');
+            }
+        } 
+    }
+
+    // 初始化
+    function init() {
+        main.innerHTML = '';
+        menu.innerHTML = '';
+
+        fetch(GROUP)
+            .then(function (response) {
+                if (response.ok) {
+                    let json = response.json();
+                    // 全局变量清空
+                    Data = []
+
+                    json.then(function(data) {  
+                        data.map(function (group) {
+                            addMenu(group.name, group.mark);
+                            addGroup(group);
+                            // 将网站数据添加到全局变量，用于搜索
+                            let siteArr = group.data;
+
+                            siteArr.map(function (site) {
+                                Data.push(site);
+                            });
+                        })
+                    });
+                }
+            })
+            .catch(function (error) {
+                console.log(JSON.stringify(error));
+            });
+    }
+
+
+    // 添加快捷导航
+    function addMenu(name, mark) {
+        let li = createNode('li'),
+            a  = createNode('a');
+
+        a.innerHTML = name;
+        a.href      = '#' + mark;
+        
+        append(li, a);
+        append(menu, li);
+    }
+
+    // 添加分组
+    function addGroup(group) {
+        // 先添加分组名称
+        addGroupName(group);
+        // 再添加网站
+        let siteArr = group.data;
+
+        siteArr.map(function (site, i) {
+            if (i % 4 == 0) {
+                let div       = createNode('div');
+                div.className = 'columns';
+
+                append(main, div);  
+            }
+
+            addGroupSite(site);
+        });
+        // 补全列
+        let groupArr = main.children;
+
+        for (let i = 0; i < groupArr.length; i++) {
+            if (groupArr[i].nodeName != 'DIV') {
+                continue;
+            }
+
+            let length = groupArr[i].children.length;
+
+            if (length >= 4) {
+                continue;
+            }
+
+            let emptyCount = 4 - length;
+
+            for (let k = 0; k < emptyCount; k++) {
+                let el = addEmpty();
+                append(groupArr[i], el); 
+            }
+        }
+    }
+
+    // 添加分组名称
+    function addGroupName(group) {
+        let h1 = createNode('h1');
+
+        h1.className = 'title up';
+        h1.innerHTML = group.name;
+        h1.id        = group.mark;
+        // 收起 - 展开事件
+        h1.addEventListener('click', function () {
+            let el = this;
+
+            while (el) { 
+                el = el.nextElementSibling;
+
+                if (! el || el.nodeName == 'H1') {
+                    break;
+                }
+
+                el.classList.toggle('is-hidden');
+            }
+        });
+        // 数量
+        let span       = createNode('span');
+        span.className = 'is-size-6 has-text-grey-light';
+        span.innerHTML = group.data.length;
+
+        append(h1, span);
+
+        append(main, h1);
+    }
+
+    // 添加网站
+    function addGroupSite(site) {
+        let group = main.children;
+
+        for (let i = 0; i < group.length; i++) {
+            if (group[i].nodeName != 'DIV') {
+                continue;
+            }
+
+            if (group[i].children.length >= 4) {
+                continue;
+            }
+
+            if (site.icon === undefined) {
+                site.icon = 'far fa-circle';
+            }
+
+            if (site.word === undefined) {
+                site.word = '';
+            }
+
+            let html = '<div class="tags has-addons are-medium" title="' + site.word + '"><span class="tag"><span class="icon is-medium"><i class="' + site.icon + '"></i></span></span><span class="tag is-rounded is-primary"><a href="' + site.href + '" class="has-text-white" target="_blank">' + site.name + '</a></span></div>';
+            let div  = createNode('div');
+
+            div.className = 'column';
+            div.innerHTML = html;
+
+            append(group[i], div); 
+        }
+    }
+
+    //
+    function addEmpty() {
+        let div       = createNode('div');
+
+        div.className = 'column';
+
+        return div;  
+    }
+
+    // 创建节点
+    function createNode(element) {
+        return document.createElement(element);
+    }
+
+    // 追加节点
+    function append(parent, el) {
+        return parent.appendChild(el);
+    }
+});
---
+++ Awesome/gulpfile.js
@@ -0,0 +1,80 @@
+let gulp         = require('gulp');
+let del          = require('del');
+let miniCss      = require('gulp-clean-css');         // Css 文件压缩
+let concat       = require('gulp-concat');            // 多个文件合并为一个
+let rev          = require('gulp-rev');               // 对文件名加MD5后缀
+let revCollector = require('gulp-rev-collector');     // 路径替换
+let htmlReplace  = require('gulp-html-replace');      // Html 替换
+let uglify       = require('gulp-uglify-es').default; // JavaScript 文件压缩
+
+
+// Font Awesome 字体资源
+gulp.task('font', function() {
+    gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest('./dist/webfonts'))
+});
+
+// Site 文件
+gulp.task('json', function() {
+    gulp.src('./site.json')
+        .pipe(rev())
+        .pipe(gulp.dest('.'))
+        .pipe(rev.manifest({
+            path: 'json-manifest.json'
+        }))
+        .pipe(gulp.dest('./dist/rev'))
+});
+
+// Css 文件
+gulp.task('awesome-css', function() {
+    gulp.src(['./node_modules/bulma/css/bulma.css', './node_modules/@fortawesome/fontawesome-free/css/all.css', './Awesome.css'])
+        .pipe(concat('awesome.css'))
+        .pipe(miniCss())
+        .pipe(rev())
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(rev.manifest({
+          path: 'awesome-css-manifest.json'
+        }))
+        .pipe(gulp.dest('./dist/rev'));
+});
+
+// JavaScript 文件
+gulp.task('awesome-script', function() {                         
+    gulp.src(['./node_modules/fuse.js/dist/fuse.js', './Awesome.js'])
+        .pipe(uglify())
+        .pipe(concat('awesome.js'))
+        .pipe(rev())
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(rev.manifest({
+          path: 'awesome-script-manifest.json'
+        }))
+        .pipe(gulp.dest('./dist/rev'))
+});
+
+gulp.task('rev', function() {
+    gulp.src(['./dist/rev/*.json', './index.html'])
+        .pipe(htmlReplace({
+            'awesome-css': '/Awesome/dist/css/awesome.css',
+            'awesome-script': '/Awesome/dist/js/awesome.js',
+            'group-json': '<script>const GROUP = "/Awesome/site.json";</script>'
+        }))
+        .pipe(revCollector())
+        .pipe(gulp.dest('../'))
+});
+
+// 清除
+gulp.task('clean', function() {
+    del
+    (
+        [
+            'dist',
+            'site-*.json'
+        ]
+    )
+});
+
+gulp.task('default', ['awesome-css', 'font', 'awesome-script', 'json']);
+
+// gulp clean
+// gulp
+// gulp rev
